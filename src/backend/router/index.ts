835feb61608d2dd4a0bf652b8de8434ebc78bb0b
Jonas Karlsson <jonas@jonaskarlsson.dev>
2022-12-12 20:49:05 +0100

    clean t3 install

--- src/backend/router/index.ts
+++
@@ -1,8 +0,0 @@
-import { repoRouter } from "./repo";
-import { createRouter } from "./context";
-
-export const appRouter = createRouter()
-  .merge(repoRouter);
-
-// export type definition of API
-export type AppRouter = typeof appRouter;
